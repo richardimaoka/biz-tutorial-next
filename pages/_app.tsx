f23985c86ac634f4e8c8599fbc4aa09ec5322c6b
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-16 22:19:23 +0900

    apollo client initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { AppProps } from "next/app";
 import "../styles/global.css";
+
+const client = new ApolloClient({
+  uri: "file://schema.gql",
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
