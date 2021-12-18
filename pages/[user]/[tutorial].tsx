d3e2bbbea45a6ab89d066ae9d7f74ca0af639863
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-18 15:54:57 +0900

    GetStaticPaths typing

--- pages/[user]/[tutorial].tsx
+++ pages/[user]/[tutorial].tsx
@@ -1,3 +1,12 @@
+import { GetStaticPaths } from "next";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { aaa: "1" } }],
+    fallback: true,
+  };
+};
+
 const Id = (): JSX.Element => {
   return <div>Id</div>;
 };
