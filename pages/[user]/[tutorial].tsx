a2c7ef236e478e289a1931862842851f562024ff
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-18 20:25:37 +0900

    getStaticProps

--- pages/[user]/[tutorial].tsx
+++ pages/[user]/[tutorial].tsx
@@ -1,8 +1,30 @@
-import { GetStaticPaths } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
+import path from "path";
+import fs from "fs";
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log("getStaticProps called");
+  return { props: {} };
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  let pathParams = [];
+  const userDirs = fs.readdirSync(path.resolve("public", "tutorial-data"));
+  for (const userDirName of userDirs) {
+    const fileNames = fs.readdirSync(
+      path.resolve("public", "tutorial-data", userDirName)
+    );
+
+    //collect all dirs and files into pathParams
+    for (const fileName of fileNames) {
+      pathParams.push({ params: { user: userDirName, tutorial: fileName } });
+    }
+  }
+
+  console.log(pathParams);
+
   return {
-    paths: [{ params: { aaa: "1" } }],
+    paths: pathParams,
     fallback: true,
   };
 };
