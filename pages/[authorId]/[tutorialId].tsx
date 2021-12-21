8fe9289fc3e2ac6e594b3851764daf42494aae9f
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-12-21 23:27:56 +0900

    param types in getStaticProps

---
+++ pages/[authorId]/[tutorialId].tsx
@@ -0,0 +1,61 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import path from "path";
+import fs from "fs";
+import { ParsedUrlQuery } from "querystring";
+
+interface StaticProps extends ParsedUrlQuery {
+  authorId: string;
+  tutorialId: string;
+}
+
+export const getStaticProps: GetStaticProps<StaticProps, StaticProps> = async ({
+  params,
+}) => {
+  console.log("params", params);
+  if (params) {
+    return {
+      props: { ...params },
+    };
+  } else {
+    throw new Error(`error in getStaticProps(): ${params}`);
+  }
+};
+
+interface StaticPathParams {
+  params: StaticProps;
+}
+// return type: { paths: StaticPathParams[], fallback: boolean }
+export const getStaticPaths: GetStaticPaths<StaticProps> = async () => {
+  let pathParams: StaticPathParams[] = [];
+  // `pathParams` will be = [
+  //    { params: { authorId: "richardimaoka", tutorialId: fileName } },
+  //    { params: { authorId: "richardimaoka", tutorialId: fileName } },
+  //    { params: { authorId: "richardimaoka", tutorialId: fileName } }
+  // ]
+
+  const authorDirs = fs.readdirSync(path.resolve("public", "tutorial-data"));
+  for (const authorDirName of authorDirs) {
+    const tutorialDirNames = fs.readdirSync(
+      path.resolve("public", "tutorial-data", authorDirName)
+    );
+
+    //collect all dirs and files into pathParams
+    for (const tutorialDirName of tutorialDirNames) {
+      pathParams.push({
+        params: { authorId: authorDirName, tutorialId: tutorialDirName },
+      });
+    }
+  }
+
+  return {
+    paths: pathParams,
+    fallback: false,
+  };
+};
+
+const Id = (params: any): JSX.Element => {
+  console.log("Id params", params);
+  return <div>Id</div>;
+};
+
+export default Id;
